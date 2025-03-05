c6c75c8f90a7277dbe527cef34aabbf08946d05f
Roharx <balint.imal@gmail.com>
2025-03-05 14:26:53 +0100

    Added initial schema migration

---
+++ StudentManagement/Models/Enrollment.cs
@@ -0,0 +1,13 @@
+namespace StudentManagement.Models
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public string? Grade { get; set; }
+
+        public Student Student { get; set; } = null!;
+        public Course Course { get; set; } = null!;
+    }
+}
