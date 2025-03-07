27f958b6640f10390cae3d956651ca1387c1ca98
Roharx <balint.imal@gmail.com>
2025-03-07 11:15:27 +0100

    Added InstructorId and Instructor (navigation property) to Course.

--- StudentManagement/Models/Course.cs
+++ StudentManagement/Models/Course.cs
@@ -6,6 +6,9 @@ namespace StudentManagement.Models
         public string Title { get; set; } = string.Empty;
         public int Credits { get; set; }
 
+        public int? InstructorId { get; set; }  //Added in: feat/new-instructor-and-relation-ef
+        public Instructor? Instructor { get; set; }  //Added in: feat/new-instructor-and-relation-ef
+
         public ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
     }
 }
