d32e171c0ceec7da3b2f95b2bb0dab5021a7f12b
Roharx <balint.imal@gmail.com>
2025-03-07 11:40:12 +0100

    Added sql file for change.

---
+++ StudentManagement/Migrations/V5__AddFinalGradeColumn.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Enrollments] ADD [FinalGrade] nvarchar(max) NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250307103244_AddFinalGradeColumn', N'9.0.2');
+
+COMMIT;
+GO
+
