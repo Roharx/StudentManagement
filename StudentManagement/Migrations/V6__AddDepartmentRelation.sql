2d918f75ffa5f9c4a20bd06c9ad5a678f97372f7
Roharx <balint.imal@gmail.com>
2025-03-07 12:28:27 +0100

    Added sql for migration

---
+++ StudentManagement/Migrations/V6__AddDepartmentRelation.sql
@@ -0,0 +1,24 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Enrollments] ADD [FinalGrade] nvarchar(max) NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250307103244_AddFinalGradeColumn', N'9.0.2');
+
+CREATE TABLE [Department] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Budget] decimal(18,2) NOT NULL,
+    [StartDate] datetime2 NOT NULL,
+    [DepartmentHeadId] int NULL,
+    CONSTRAINT [PK_Department] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Department_Instructor_DepartmentHeadId] FOREIGN KEY ([DepartmentHeadId]) REFERENCES [Instructor] ([Id])
+);
+
+CREATE UNIQUE INDEX [IX_Department_DepartmentHeadId] ON [Department] ([DepartmentHeadId]) WHERE [DepartmentHeadId] IS NOT NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250307112732_AddDepartmentRelation', N'9.0.2');
+
+COMMIT;
+GO
+
