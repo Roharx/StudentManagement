b15e8a22b80c1706325e8fc658ed298246d2f87b
Roharx <balint.imal@gmail.com>
2025-03-07 10:28:00 +0100

    Added date of birth to students, added comments on which brances were specific columns added.

---
+++ StudentManagement/Migrations/V3__AddDateOfBirthToStudent.sql
@@ -0,0 +1,14 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Students] ADD [MiddleName] nvarchar(max) NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250307090942_AddMiddleNameToStudent', N'9.0.2');
+
+ALTER TABLE [Students] ADD [DateOfBirth] datetime2 NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250307092619_AddDateOfBirthToStudent', N'9.0.2');
+
+COMMIT;
+GO
+
--- StudentManagement/Models/Student.cs
+++ StudentManagement/Models/Student.cs
@@ -4,11 +4,11 @@ namespace StudentManagement.Models
     {
         public int Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
-        public string? MiddleName { get; set; } //Non-destructive since nullable
+        public string? MiddleName { get; set; } //Added in: feat/add-student-middlename-ef
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public DateTime EnrollmentDate { get; set; }
-        
+        public DateTime? DateOfBirth { get; set; } //Added in: feat/add-student-date-of-birth-ef
         public ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
     }
 }
