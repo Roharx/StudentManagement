98111250861f2377d5bfa87c5c2bfd32e24fab2a
Roharx <balint.imal@gmail.com>
2025-03-07 11:22:24 +0100

    Generated migrations for the new changes (new: instructor, altered: course, studentContext)

---
+++ StudentManagement/Migrations/V4__AddInstructorRelation.sql
@@ -0,0 +1,22 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Courses] ADD [InstructorId] int NULL;
+
+CREATE TABLE [Instructor] (
+    [Id] int NOT NULL IDENTITY,
+    [FirstName] nvarchar(max) NOT NULL,
+    [LastName] nvarchar(max) NOT NULL,
+    [Email] nvarchar(max) NOT NULL,
+    [HireDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_Instructor] PRIMARY KEY ([Id])
+);
+
+CREATE INDEX [IX_Courses_InstructorId] ON [Courses] ([InstructorId]);
+
+ALTER TABLE [Courses] ADD CONSTRAINT [FK_Courses_Instructor_InstructorId] FOREIGN KEY ([InstructorId]) REFERENCES [Instructor] ([Id]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250307101720_AddInstructorRelation', N'9.0.2');
+
+COMMIT;
+GO
+
