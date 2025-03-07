76fc9a47ccb9e8d3eb35a144ce81c123d9f40e51
Roharx <balint.imal@gmail.com>
2025-03-07 10:17:45 +0100

    Added middle name to students, nullable (non-destructive)

---
+++ StudentManagement/Migrations/V2__AddMiddleNameToStudent.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Students] ADD [MiddleName] nvarchar(max) NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250307090942_AddMiddleNameToStudent', N'9.0.2');
+
+COMMIT;
+GO
+
--- StudentManagement/Models/Student.cs
+++ StudentManagement/Models/Student.cs
@@ -4,6 +4,7 @@ namespace StudentManagement.Models
     {
         public int Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
+        public string? MiddleName { get; set; } //Non-destructive since nullable
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public DateTime EnrollmentDate { get; set; }
