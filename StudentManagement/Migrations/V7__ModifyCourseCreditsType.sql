f4c5d7e3b288dc922f9190b2eaa5bbadc8168f2c
Roharx <balint.imal@gmail.com>
2025-03-07 12:46:10 +0100

    Created sql.

---
+++ StudentManagement/Migrations/V7__ModifyCourseCreditsType.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Courses] ADD [CreditsDecimal] decimal(18,2) NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250307114004_AddDecimalCreditsToCourse', N'9.0.2');
+
+COMMIT;
+GO
+
